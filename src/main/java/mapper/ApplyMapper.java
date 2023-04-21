0e319beb18a02afcd53fdc1ae48e782b73806b00
gusqls7748 <gusqls7748@naver.com>
2023-04-21 17:31:33 +0900

    ss
    ss

--- src/main/java/mapper/ApplyMapper.java
+++ src/main/java/mapper/ApplyMapper.java
@@ -15,12 +15,15 @@ public interface ApplyMapper {
 	// insert, update, delete => int
 	// select => list, object int ...다양함.
 	
+	
+	// 수업 등록을 위한 값
 	@Insert({
 		" INSERT INTO Apply (person, memberid, sessionno)",
 		" VALUES(#{obj.person}, #{obj.memberid}, #{obj.sessionno}"
 	})
 	public int insertApply(@Param("obj") Apply obj);
 	
+	// 등록한 것을 보기 위한 값
 	@Select({
 		"Select FROM Apply WHERE memberid=#{id} "
 	})
--- src/main/java/mapper/ApplyStatusMapper.java
+++ src/main/java/mapper/ApplyStatusMapper.java
@@ -12,12 +12,15 @@ public interface ApplyStatusMapper {
 	// insert, update, delete => int
 	// select => list, object int ...다양함.
 	
+	// 신청 상태 뭘 들고올지 
 	@Insert({
 		" INSERT INTO APPLYTATUS (applyno,chk,regdate) ",
 		" value(#{obj.applyno},#{obj.chk},#{obj.regdate})"
 	})
 	public int insertApplyStatus(@Param("obj")ApplyStatus obj);
 	
+	
+	// 신청상태에서 보기위해 뷰를 만들어서 보기 위한 것
 //	@Select({
 //		"select av.* FROM Applytatusview av WHERE applyno=#{no}"
 //	})
