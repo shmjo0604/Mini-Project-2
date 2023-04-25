aa9400175358e8bdf1c6e4ba91fb0c07a75d1c25
Leejaeho9477 <123917210+Exit-Chokey@users.noreply.github.com>
2023-04-25 20:49:16 +0900

    8시49분

--- src/main/java/controller/review/SelectViewController.java
+++ src/main/java/controller/review/SelectViewController.java
@@ -20,22 +20,7 @@ public class SelectViewController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-	
-//		
-//		System.out.println(no);
-//		
-		ClassSessionView obj = new ClassSessionView();
-		obj.setNo(0);
-		System.out.println(obj.toString());
-//		
-//		ClassSessionViewMapper mapper = MyBatisContext.getSqlSession().getMapper(ClassSessionViewMapper.class);
-//		ClassSessionView view = mapper.ClassSessionViewSelectOne(obj);
-		
-		ClassSessionViewMapper mapper = MyBatisContext.getSqlSession().getMapper(ClassSessionViewMapper.class);
-		ClassSessionView int1 = mapper.aa();
-				
-				
-		request.setAttribute("view", int1);
+
 		
 		request.getRequestDispatcher("/WEB-INF/review/selectview.jsp").forward(request, response);
 	}
