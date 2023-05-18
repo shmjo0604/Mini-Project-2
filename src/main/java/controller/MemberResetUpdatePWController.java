125b31f1300038bafc3dfc2a48de07cd146320f1
shmjo0604 <shmjo0604@gmail.com>
2023-05-18 15:25:17 +0900

    1
    1

--- src/main/java/controller/MemberResetUpdatePWController.java
+++ src/main/java/controller/MemberResetUpdatePWController.java
@@ -24,7 +24,7 @@ import dto.Member;
 public class MemberResetUpdatePWController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-		private MemberService mService =new MemberServiceImpl();
+		private MemberService mService = new MemberServiceImpl();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
